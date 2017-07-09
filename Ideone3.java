f73fad0f90497d701ebf6170c15fc6e6ee89738f
nehasinha703 <nehasinha703@gmail.com>
2017-07-09 16:10:26 +0530

    Add files via upload

---
+++ Ideone3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{/* Name of the class has to be "Main" only if the class is public. */
+class Ideone3
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+Scanner sc=new Scanner(System.in);
+String str=sc.next();
+char ch=str.charAt(0);
+if ((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+System.out.println("it is vowel");
+else
+System.out.println("it is consonant");
+}
+    }
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner sc=new Scanner(System.in);
+String str=sc.next();
+char ch=str.charAt(0);
+if ((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+System.out.println("it is vowel");
+else
+System.out.println("it is consonant");
+}
+	}
