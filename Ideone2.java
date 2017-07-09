68446ac3fa37009e5c4df65ca44cc03aa3a66118
nehasinha703 <nehasinha703@gmail.com>
2017-07-09 16:09:49 +0530

    Add files via upload

---
+++ Ideone2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner s=new Scanner(System.in);
+System.out.println("enter no");
+int n=s.nextInt();
+if (n%2==0)
+System.out.println("no is even");
+else
+System.out.println("no is odd");
+	}
+}
