8153941a41e8f77b48c2752b3cf7f379a2ac3f97
nehasinha703 <nehasinha703@gmail.com>
2017-07-09 16:12:43 +0530

    Add files via upload

---
+++ Ideone9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone9
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+int sum = 0;
+int n=10;
+for(int i = 1;i <= n;i++)
+{
+sum = sum + i;
+}
+System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+}
+}
